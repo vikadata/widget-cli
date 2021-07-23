439898f5c4c385ee1686ee469e0dcdf5e72f4aa7
陈加贝 <chenjiabei@vikadata.com>
2021-07-23 15:08:06 +0800

    test: debug mode provide more request infomation

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -6,6 +6,13 @@ const hook: Hook<'init'> = async function(options) {
   axios.interceptors.response.use(function(response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    if (process.env.DEBUG) {
+      console.log(chalk.redBright('Response Debug:'));
+      console.log({
+        config: response.config,
+        responseData: response.data,
+      });
+    }
     return response;
   }, function(error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
