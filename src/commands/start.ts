9204511100dcfa3fb5fcc6acff2e89333905e5a7
陈加贝 <chenjiabei@vikadata.com>
2021-08-05 16:22:36 +0800

    feat: support https dev server

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,10 +1,14 @@
-/* eslint-disable no-console */
 import { Command, flags } from '@oclif/command';
+import * as https from 'https';
+import * as http from 'http';
+import * as fse from 'fs-extra';
 import * as chalk from 'chalk';
 import * as path from 'path';
 import * as express from 'express';
 import Config from '../config';
 import { startCompile } from '../utils/project';
+
+const sslDir = path.resolve(__dirname, '../../ssl');
 
 export default class Start extends Command {
   static description = 'start current widget project in develop mode';
@@ -16,25 +20,42 @@ Compiling...
   ];
 
   static flags = {
-    port: flags.string({ char: 'p', description: 'Specifies the port of the local server', default: '9000' }),
+    port: flags.string({ char: 'p', description: 'specifies the port of the local server', default: '9000' }),
+    protocol: flags.string({ char: 'o', description: 'specifies the protocol of the local server', default: 'https' }),
     debug: flags.boolean({ description: 'show debug information for cli it self' }),
   };
 
-  hostCompliedFile(port: string) {
+  hostCompliedFile(port: string, protocol: string) {
     const app = express();
-    app.listen(port);
+
+    if (protocol === 'https') {
+      const privateKey = fse.readFileSync(path.resolve(sslDir, 'server.key'), 'utf8');
+      const certificate = fse.readFileSync(path.resolve(sslDir, 'server.crt'), 'utf8');
+      const credentials = { key: privateKey, cert: certificate };
+
+      https.createServer(credentials, app).listen(port);
+    } else {
+      http.createServer(app).listen(port);
+    }
+
     app.use(express.static(path.join(Config.releaseCodePath)));
   }
 
   async run() {
-    const { flags: { port }} = this.parse(Start);
+    const { flags: { port, protocol }} = this.parse(Start);
+    let firstCompile = true;
 
     startCompile('dev', () => {
-      this.log(chalk.cyanBright('************************'));
-      this.log(chalk.yellowBright('Copy the following address and paste it into the developing widget container:'));
-      this.log(chalk.yellowBright(`http://localhost:${port}/${Config.releaseCodeName}`));
-      this.log(chalk.cyanBright('************************'));
+      if (firstCompile) {
+        this.log(chalk.cyanBright('************************'));
+        this.log(chalk.yellowBright('Copy the following address and paste it into the developing widget container:'));
+        this.log(chalk.yellowBright(`${protocol}://localhost:${port}/${Config.releaseCodeName}`));
+        this.log(chalk.cyanBright('************************'));
+      } else {
+        this.log('Code has been recompiled');
+      }
+      firstCompile = false;
     });
-    this.hostCompliedFile(port);
+    this.hostCompliedFile(port, protocol);
   }
 }
