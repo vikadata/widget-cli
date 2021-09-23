0112c9efea3610aaaf3c00de1ca6cca534716923
陈加贝 <chenjiabei@vikadata.com>
2021-09-23 14:26:15 +0800

    fix: hide admin command

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -4,6 +4,8 @@ import * as chalk from 'chalk';
 export default class Link extends Command {
   static description = 'Link a space widget to another space (developing)';
 
+  static hidden = true;
+
   static examples = [
     `$ widget-cli link
 Succeed!
@@ -11,7 +13,7 @@ Succeed!
   ];
 
   async run() {
-    this.log(chalk.greenBright('This command is developing, you can init a new widget to \
-the space that you want to transform to, then copy the source code from existing widget and release it'));
+    this.log(chalk.greenBright(`This command is developing, you can init a new widget to
+the space that you want to transform to, then copy the source code from existing widget and release it`));
   }
 }
