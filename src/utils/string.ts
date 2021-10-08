5f29496234d465e817b1477062980d99fbecd75c
彭安平 <penganping@vikadata.com>
2021-10-08 18:53:01 +0800

    fix: list release format output

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,1 +1,9 @@
 export const kebab2camel = (s: string) => s.replace(/-./g, x => x.toUpperCase()[1]);
+
+export const wrapOutput = (s: string, strLength?: number) => {
+    strLength = strLength || 10;
+    const regObj = new RegExp(/[^\x00-\xff]/, 'g');
+    const regObj1 = new RegExp('.{' + strLength + '}\\x01?', 'g');
+    const regObj2 = new RegExp(/\x01/, 'g');
+    return s.replace(regObj, '$&\x01').replace(regObj1, '$&\n').replace(regObj2, '');
+};
