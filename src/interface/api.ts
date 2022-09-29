c20c4e8903a727a8c03e6e1f4fa256e944031486
wangbo <wangbo@vikadata.com>
2022-09-29 19:28:46 +0800

    feat: assets upload

--- src/interface/api.ts
+++ src/interface/api.ts
@@ -1,15 +1,6 @@
-import { EUploadType } from './api_dict_enum';
-
 export interface IApiWrapper<T = any> {
   code: number;
   message: string;
   success: boolean;
   data: T;
-}
-
-export interface IApiUploadAuth {
-  uploadToken: string;
-  resourceKey: string;
-  uploadType: EUploadType;
-  endpoint: string;
 }
